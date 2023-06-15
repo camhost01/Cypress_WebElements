ff157f9cb6bbed6b678fa17f4b223166ac34b7cf
camhost01 <39304271+camhost01@users.noreply.github.com>
2023-06-15 17:44:09 -0500

    Refactor scenario name iFrame

--- cypress/e2e/Test/iFrames/testCases.cy.js
+++ cypress/e2e/Test/iFrames/testCases.cy.js
@@ -3,7 +3,7 @@
 import datatest from '/cypress/fixtures/dataTest.json'
 import actions from '../../Actions/iframe/actions'
 
-describe('Test escenarios swag_lab', ()=>{
+describe('Test escenarios iFRame - nested iFrame', ()=>{
 
     beforeEach(()=>{
         cy.visit(datatest.iFrameURL)

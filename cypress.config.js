408b8b7ad69304b8a9f0c2dd0603e1d219f4b309
camhost01 <39304271+camhost01@users.noreply.github.com>
2023-10-05 21:09:15 -0500

    adding mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("cypress");
 const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   chromeWebSecurity: false,
   e2e: {
     defaultCommandTimeout: 10000,
@@ -11,6 +13,15 @@ module.exports = defineConfig({
     viewportHeight: 1080,
     setupNodeEvents(on, config) {
       addMatchImageSnapshotPlugin(on, config)
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        await beforeRunHook(details);
+        });
+  
+        on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+        });
     },
   },
 });
